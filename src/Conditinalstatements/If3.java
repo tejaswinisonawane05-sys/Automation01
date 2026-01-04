4f4550dfc70188ed46fffd0493f2cf8c752a689a
tejaswinisonawane05 <tejaswini.sonawane05@gmail.com>
2026-01-04 15:02:08 +0530

    for loop

--- src/Conditinalstatements/If3.java
+++ src/Conditinalstatements/If3.java
@@ -1,4 +1,4 @@
-package CoreJava;
+package Conditinalstatements;
 
 public class If3 {
 
---
+++ src/Iterationstatements/Whileloop3.java
@@ -0,0 +1,15 @@
+package Iterationstatements;
+
+public class Whileloop3 {
+
+	public static void main(String[] args) {
+				int i=1;
+		while (i<=10) {
+			int square=i*i;
+			System.out.println("sqaure of "+ i+"="+square);
+			i++;
+		}
+
+	}
+
+}
